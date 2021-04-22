a5f067ccc2eda6644b599828a0831e0587e67f5d
Natali <ns@numbuster.com>
2021-04-22 13:18:45 +0300

    Added saving to CoreData

--- HitList/ViewController.swift
+++ HitList/ViewController.swift
@@ -6,14 +6,76 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    //NSManagedObject represents a single object stored in Core Data
+    var people: [NSManagedObject] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "The List"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
+    
+    // Implement the addName IBAction
+    @IBAction func addName(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "New Name",
+                                      message: "Add a new name",
+                                      preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { [unowned self] action in
+            guard let textField = alert.textFields?.first,
+                  let nameToSave = textField.text else {
+                return
+            }
+            self.save(name: nameToSave)
+            self.tableView.reloadData()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .cancel)
+        alert.addTextField()
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
+    
+    // Save to coreData
+    func save(name: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        // 1- generated a managed object context (already done in AppDelegate)
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        // 2 - create a new managed object and insert it into the managed object context
+        let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)!
+        let person = NSManagedObject(entity: entity, insertInto: managedContext)
+        // 3 - set the name attribute using key-value coding. It must spell the KVC key (name in this case) exactly as it appears in Data Model, otherwise, app will crash at runtime
+        person.setValue(name, forKeyPath: "name")
+        // 4 - “commit” the changes in managed object context to save it to disk
+        do {
+            try managedContext.save()
+            people.append(person)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+}
 
-
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let person = people[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        // grab the name attribute from the NSManagedObject
+        cell.textLabel?.text = person.value(forKeyPath: "name") as? String
+        return cell
+        
+    }
 }
 
